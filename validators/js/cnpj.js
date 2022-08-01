78d011e330ac553d10c8546a8876ac5778fd83b6
Dev One - soluções em software <marcosdevelopment.business@gmail.com>
2022-08-01 09:34:52 -0300

    add initial js validators

---
+++ validators/js/cnpj.js
@@ -0,0 +1,54 @@
+export function vlaidateCNPJ(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g, "");
+
+  if (cnpj === "") return false;
+
+  if (cnpj.length !== 14) return false;
+
+  const commonCNPJ = [
+    "00000000000000",
+    "11111111111111",
+    "22222222222222",
+    "33333333333333",
+    "44444444444444",
+    "55555555555555",
+    "66666666666666",
+    "77777777777777",
+    "88888888888888",
+    "99999999999999",
+  ];
+
+  if (commonCNPJ.some((ccnpj) => ccnpj === cnpj)) return false;
+
+  // Valida DVs
+  let tamanho = cnpj.length - 2;
+  let numeros = cnpj.substring(0, tamanho);
+  let digitos = cnpj.substring(tamanho);
+  let soma = 0;
+  let pos = tamanho - 7;
+
+  for (let i = tamanho; i >= 1; i--) {
+    soma += Number(numeros.charAt(tamanho - i)) * pos--;
+    if (pos < 2) pos = 9;
+  }
+
+  let resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+  if (resultado !== Number(digitos.charAt(0))) return false;
+
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0, tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+
+  for (let i = tamanho; i >= 1; i--) {
+    soma += Number(numeros.charAt(tamanho - i)) * pos--;
+    if (pos < 2) pos = 9;
+  }
+
+  resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+  if (resultado !== Number(digitos.charAt(1))) return false;
+
+  return true;
+}
---
+++ validators/js/cpf.js
@@ -0,0 +1,62 @@
+/**
+ * verify if cpf is valid
+ * @param {String} cpf string with 11 numbers without any other char else number
+ * @returns {boolean} true or false
+ */
+export function validateCPF(cpf) {
+  const nineFirstNumbers = cpf.slice(0, 9).split("");
+  const digitVerify = cpf.slice(9);
+
+  const nineFirstNumbersMultiplied = nineFirstNumbers.map(
+    (n, i) => Number(n) * (10 - i)
+  );
+
+  let sumNineFirstNumbers = 0;
+
+  for (let i = 0; i < 9; i++) {
+    sumNineFirstNumbers += Number(nineFirstNumbersMultiplied[i]);
+  }
+
+  const c1 = (sumNineFirstNumbers * 10) % 11;
+  let n1;
+
+  if (c1 === 10 || c1 === 11) {
+    n1 = "0";
+  } else {
+    n1 = String(c1);
+  }
+
+  if (n1 !== digitVerify[0]) {
+    return false;
+  }
+
+  // -
+
+  const tenFirstNumbers = cpf.slice(0, 10).split("");
+  const tenFirstNumbersMultiplied = tenFirstNumbers.map(
+    (n, i) => Number(n) * (11 - i)
+  );
+  let sumTenFirstNumber = 0;
+
+  for (let i = 0; i < 10; i++) {
+    sumTenFirstNumber += Number(tenFirstNumbersMultiplied[i]);
+  }
+
+  const c2 = (sumTenFirstNumber * 10) % 11;
+  let n2;
+
+  if (c2 === 10 || c2 === 11) {
+    n2 = "0";
+  } else {
+    n2 = String(c2);
+  }
+
+  if (n2 !== digitVerify[1]) {
+    return false;
+  }
+  if (cpf === "00000000000") {
+    return false;
+  }
+
+  return true;
+}
