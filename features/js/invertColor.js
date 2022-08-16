e5150e2bf027c3ad3c4fb0d0585a21088d394ebe
Dev One - soluções em software <marcosdevelopment.business@gmail.com>
2022-08-16 17:27:58 -0300

    add feature to invert color

---
+++ features/js/invertColor.js
@@ -0,0 +1,15 @@
+import { validateHexColor } from "../../validators/js/hex-color";
+
+export function invertColor(hex) {
+  hex = hex.replace("#", "");
+
+  if (!validateHexColor(hex)) {
+    throw new Error("Invalid hex color: " + hex);
+  }
+
+  const r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16);
+  const g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16);
+  const b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+
+  return "#" + r.padStart(2, "0") + g.padStart(2, "0") + b.padStart(2, "0");
+}
