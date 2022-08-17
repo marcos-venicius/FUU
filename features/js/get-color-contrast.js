406ae2dfd4680af75cc81163c05cf29dcf65240a
Dev One <94018427+dev-one-company@users.noreply.github.com>
2022-08-17 14:49:41 -0300

    Create get-color-contrast.js

---
+++ features/js/get-color-contrast.js
@@ -0,0 +1,11 @@
+export function getColorContrast(hexColor) {
+  hexColor = hexColor.replace('#', '');
+
+  const r = parseInt(hexColor.substr(0, 2), 16);
+  const g = parseInt(hexColor.substr(2, 2), 16);
+  const b = parseInt(hexColor.substr(4, 2), 16);
+
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+
+  return yiq >= 128 ? '#000000' : '#ffffff';
+}
