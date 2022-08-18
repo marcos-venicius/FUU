b3a67a19f8cbddde0f012fb3f88193c9b9f91e63
Dev One <94018427+dev-one-company@users.noreply.github.com>
2022-08-18 16:41:05 -0300

    Update get-color-contrast.js

--- features/js/get-color-contrast.js
+++ features/js/get-color-contrast.js
@@ -1,9 +1,27 @@
 export function getColorContrast(hexColor) {
   hexColor = hexColor.replace('#', '');
 
-  const r = parseInt(hexColor.substr(0, 2), 16);
-  const g = parseInt(hexColor.substr(2, 2), 16);
-  const b = parseInt(hexColor.substr(4, 2), 16);
+  let r = 0;
+  let g = 0;
+  let b = 0;
+
+  if (hexColor.length === 8) {
+    hexColor = hexColor.substring(0, 6);
+  }
+
+  if (hexColor.length === 6) {
+    r = parseInt(hexColor.substring(0, 1), 16);
+    g = parseInt(hexColor.substring(2, 3), 16);
+    b = parseInt(hexColor.substring(4, 5), 16);
+  } else if (hexColor.length === 3) {
+    const _r = hexColor[0];
+    const _g = hexColor[1];
+    const _b = hexColor[2];
+
+    r = parseInt(`${_r}${_r}`, 16);
+    g = parseInt(`${_g}${_g}`, 16);
+    b = parseInt(`${_b}${_b}`, 16);
+  }
 
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
