581aee2d1988b5840c7982f70827adafb04d4e69
Dev One <94018427+dev-one-company@users.noreply.github.com>
2022-08-18 16:40:26 -0300

    Update hex-color.js

--- validators/js/hex-color.js
+++ validators/js/hex-color.js
@@ -1,3 +1,9 @@
-export function validateHexColor(color) {
-  return /^#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/.test(String(color).trim())
+export function validateHexColor(color: string) {
+  const digits: { [key: number]: RegExp } = {
+    8: /^#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/,
+    6: /^#?([\da-fA-F]{2})([\da-fA-F]{2})([\da-fA-F]{2})$/,
+    3: /^#?([\da-fA-F]{1})([\da-fA-F]{1})([\da-fA-F]{1})$/,
+  };
+
+  return Object.keys(digits).some(key => digits[Number(key)].test(color));
 }
