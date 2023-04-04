2299cb7764843e5b300ca07c455c276e72f09bc1
Marcos Venicius <94018427+marcos-venicius@users.noreply.github.com>
2023-04-04 09:56:54 -0700

    docs to features/ts/join-generic-elements.ts

--- features/ts/join-generic-elements.ts
+++ features/ts/join-generic-elements.ts
@@ -1,3 +1,9 @@
+/**
+ * join any type of elements
+ * @param elements list of elements to be "joined"
+ * @param join the element the will be between elements
+ * @returns joined list
+ */
 function joinGenericElements<T>(elements: T[], join: T): T[] {
   if (elements.length < 2) return elements
 
