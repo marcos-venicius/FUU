679e2a6987e4180d6a3358cdc4ad44c04ccf490a
Marcos Venicius <94018427+marcos-venicius@users.noreply.github.com>
2023-05-26 09:25:48 -0300

    fix ValidateCnpj.cs

--- validators/cs/ValidateCnpj.cs
+++ validators/cs/ValidateCnpj.cs
@@ -12,11 +12,11 @@ public class ValidateCnpj
 
     public bool IsValid()
     {
-        var cnpj = new Regex("\\D").Replace(_cnpj, "");
+        var cnpj = new Regex(@"[.\-/]").Replace(_cnpj, "");
 
-        if (cnpj.Length < 14)
+        if (cnpj.Length != 14)
             return false;
-        
+
         var verifyingDigits = cnpj[^2..].Select(x => int.Parse(x.ToString())).ToList();
         var cnpjWithoutLastTwoDigits = cnpj[..^2].Select(x => int.Parse(x.ToString())).ToList();
 
@@ -27,22 +27,16 @@ public class ValidateCnpj
         var resultTwo = new int[testTwo.Length];
 
         for (var i = 0; i < testOne.Length; i++)
-        {
             resultOne[i] = cnpjWithoutLastTwoDigits[i] * testOne[i];
-        }
 
         var resultOneSum = resultOne.Sum();
 
         int verifyingDigitOne;
 
         if (resultOneSum % 11 < 2)
-        {
             verifyingDigitOne = resultOneSum;
-        }
         else
-        {
             verifyingDigitOne = 11 - resultOneSum % 11;
-        }
 
         if (verifyingDigits[0] != verifyingDigitOne)
             return false;
@@ -50,22 +44,16 @@ public class ValidateCnpj
         cnpjWithoutLastTwoDigits.Add(verifyingDigitOne);
 
         for (var i = 0; i < testTwo.Length; i++)
-        {
             resultTwo[i] = cnpjWithoutLastTwoDigits[i] * testTwo[i];
-        }
 
         var resultTwoSum = resultTwo.Sum();
 
         int verifyingDigitTwo;
 
         if (resultTwoSum % 11 < 2)
-        {
             verifyingDigitTwo = 0;
-        }
         else
-        {
             verifyingDigitTwo = 11 - resultTwoSum % 11;
-        }
 
         return verifyingDigitTwo == verifyingDigits[1];
     }
