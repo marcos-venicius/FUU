9c84ceb66fd6b293cb7a918a7ed28cf76091e4de
Marcos Venicius <94018427+marcos-venicius@users.noreply.github.com>
2023-05-31 18:50:37 -0300

    Don't conver to base 10 when the number already is in base 10

--- algorithms/py/convert-between-bases.py
+++ algorithms/py/convert-between-bases.py
@@ -1,12 +1,15 @@
 def convert_between_bases(value_to_convert: int, base_from: int, base_to: int) -> int:
   n = 0
 
-  pos = 0
-
-  while value_to_convert > 0:
-    n += (value_to_convert % 10) * base_from ** pos
-    value_to_convert //= 10
-    pos += 1
+  if base_from == 10:
+    n = value_to_convert
+  else:
+    pos = 0
+  
+    while value_to_convert > 0:
+      n += (value_to_convert % 10) * base_from ** pos
+      value_to_convert //= 10
+      pos += 1
 
   r: int = 0
   p = 0
